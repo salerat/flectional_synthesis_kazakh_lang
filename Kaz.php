1caf0b9bfe344f8c52535fff5fe15da5c4cc1e4c
salerat <salerat@gmail.com>
2014-04-03 11:47:22 +0700

    created method ontrolConsonantStartedByConsonant

--- Kaz.php
+++ Kaz.php
@@ -2,40 +2,120 @@
 
 namespace kaz;
 
-error_reporting(E_ALL | E_STRICT) ;
+error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'On');
 
-class Kaz {
+class Kaz
+{
 
     protected $mysqli;
+    protected $wordOriginal;
 
-    function __construct()
+    function getLastSyllable()
     {
-        $this->mysqli = new \mysqli("localhost", "root", "gerogle", "test");
+
+    }
+
+//возвращает 0 если следущая буква гласная
+//возвращает 1 если текущая буква согласная и последняя в слове
+//возвращает 2 если текущая буква препдпоследняя согласная, с условием того, что после следущей согласной буквы следует ГЛАСНАЯ
+//возвращает 3 если текущая буква препдпоследняя согласная, с условием того, что после следущей согласной буквы следует СОГЛАСНАЯ
+//возвращает 3 если текущая буква препдпоследняя согласная, с условием того, что после следущей согласной больше БУКВ нет
+    protected function controlConsonantStartedByConsonant($consonant, $splitedWord, $starNumber)
+    {
+        if (!empty($splitedWord[$starNumber + 1])) {
+            if (!in_array($splitedWord[$starNumber + 1], $consonant)) {
+                return 0;
+            } else {
+                if (!empty($splitedWord[$starNumber + 2])) {
+                    if (!in_array($splitedWord[$starNumber + 2], $consonant)) {
+                        return 2;
+                    } else {
+                        return 3;
+                    }
+                } else {
+                    return 4;
+                }
+            }
+        } else {
+            return 1;
+        }
+    }
+
+    public function breakToSyllable()
+    {
+
+        $result = $this->mysqli->query('SELECT word FROM vowel');
+        $vowels = array();
+        while ($row = $result->fetch_assoc()) {
+            $vowels[] = $row['word'];
+        }
+        $result = $this->mysqli->query('SELECT word FROM consonant');
+        $consonant = array();
+        while ($row = $result->fetch_assoc()) {
+            $consonant[] = $row['word'];
+        }
+        $splitedWord = str_split($this->wordOriginal);
+
+        $syllableArray = array();
+        $startedByConsonant = false;
+        $inSyllable = false;
+        $count = 0;
+        foreach ($splitedWord as $key => $word) {
+            if (!$inSyllable) {
+                if (in_array($word, $consonant)) {
+                    $startedByConsonant = true;
+                } else {
+                    $startedByConsonant = false;
+                }
+                $count++;
+                $inSyllable = true;
+                $syllableArray[$count] = '';
+            }
+
+            $syllableArray[$count] = $syllableArray[$count] . $word;
+
+            if (($startedByConsonant)) {
+                if (in_array($word, $consonant)) {
+                    $resNumber = $this->controlConsonantStartedByConsonant($consonant, $splitedWord, $key);
+                }
+            } else {
+
+            }
+
+        }
+        die(var_dump($consonant));
+    }
+
+    function __construct($wordOriginal)
+    {
+        $this->mysqli = new \mysqli("localhost", "root", "gerogle", "term_k");
         $this->mysqli->set_charset('utf8');
+        $this->wordOriginal = $wordOriginal;
     }
 
     public function test()
     {
         $query = "SELECT * FROM word_case";
-        $result=$this->mysqli->query($query);
+        $result = $this->mysqli->query($query);
         while ($row = $result->fetch_assoc()) {
             echo($row['case']);
         }
     }
 
-    public function generateEndings() {
+    public function generateEndings()
+    {
         $word = "адам";
         $query = "SELECT * FROM plural, possessive,case_letter";
-        $result=$this->mysqli->query($query);
+        $result = $this->mysqli->query($query);
         while ($row = $result->fetch_assoc()) {
-            echo($word.$row['plural'].$row['possessive'].$row['case_letter'].PHP_EOL);
+            echo($word . $row['plural'] . $row['possessive'] . $row['case_letter'] . PHP_EOL);
         }
     }
 }
 
-$kaz = new Kaz();
-$kaz->generateEndings();
+$kaz = new Kaz('адам');
+$kaz->breakToSyllable();
 
 
 /*
